d5c16d61fedb97f123befbb1bbbed0571113b4fa
Administrator <Administrator@USER-20161013UJ>
2019-08-27 16:10:12 +0800

    i

---
+++ sp01-common/src/main/java/com/tedu/sp01/Sp01CommonApplication.java
@@ -0,0 +1,13 @@
+package com.tedu.sp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp01CommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp01CommonApplication.class, args);
+	}
+
+}
